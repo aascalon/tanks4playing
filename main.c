bf0b9b67b6337045cc151c2f8bd47f6a5e8311be
aascalon <23003498+aascalon@users.noreply.github.com>
2018-06-28 17:43:08 -0400

    algorithmic tank creation

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ OS_SEM move;
 
 uint32_t moveDelay = 1000;
 
-
 unsigned short field[48][64] = {White};
  
 typedef struct{
@@ -33,38 +32,70 @@ void drawPixel(int x, int y, unsigned short colour){
 }
 
 void drawTank(void){
-	drawPixel((tank.xPos)-1, (tank.yPos)-1, Green);
-	drawPixel((tank.xPos)-1, (tank.yPos)+1, Green);
-	drawPixel((tank.xPos)+1, (tank.yPos)-1, Green);
-	drawPixel((tank.xPos)+1, (tank.yPos)+1, Green);
-	if(tank.aimDir != up)
-		drawPixel(tank.xPos, (tank.yPos)-1, Green);
-	if(tank.aimDir != down)
-		drawPixel(tank.xPos, (tank.yPos)+1, Green);
-	if(tank.aimDir != right)
-		drawPixel((tank.xPos)+1, tank.yPos, Green);
-	if(tank.aimDir != left)
-		drawPixel((tank.xPos)-1, tank.yPos, Green);
-	
+// 	drawPixel((tank.xPos)-1, (tank.yPos)-1, Green);
+// 	drawPixel((tank.xPos)-1, (tank.yPos)+1, Green);
+// 	drawPixel((tank.xPos)+1, (tank.yPos)-1, Green);
+// 	drawPixel((tank.xPos)+1, (tank.yPos)+1, Green);
+// 	if(tank.aimDir != up)
+// 		drawPixel(tank.xPos, (tank.yPos)-1, Green);
+// 	if(tank.aimDir != down)
+// 		drawPixel(tank.xPos, (tank.yPos)+1, Green);
+// 	if(tank.aimDir != right)
+// 		drawPixel((tank.xPos)+1, tank.yPos, Green);
+// 	if(tank.aimDir != left)
+// 		drawPixel((tank.xPos)-1, tank.yPos, Green);
+// 	
+ 	
+// 	
+// 	if(tank.aimDir == up){
+// 		drawPixel(tank.xPos, (tank.yPos)-1, Black);
+// 		drawPixel(tank.xPos, (tank.yPos)-2, Black);
+// 	}
+// 	else if(tank.aimDir == right){
+// 		drawPixel((tank.xPos)+1, tank.yPos, Black);
+// 		drawPixel((tank.xPos)+2, tank.yPos, Black);
+// 	}
+// 	else if(tank.aimDir == down){
+// 		drawPixel(tank.xPos, (tank.yPos)+1, Black);
+// 		drawPixel(tank.xPos, (tank.yPos)+2, Black);
+// 	}
+// 	else {
+// 		drawPixel((tank.xPos)-1, tank.yPos, Black);
+// 		drawPixel((tank.xPos)-2, tank.yPos, Black);
+// 	}
+	int8_t i,j, canX, canY;
 	drawPixel(tank.xPos, tank.yPos, Black);
-	
 	if(tank.aimDir == up){
-		drawPixel(tank.xPos, (tank.yPos)-1, Black);
-		drawPixel(tank.xPos, (tank.yPos)-2, Black);
-	}
-	else if(tank.aimDir == right){
-		drawPixel((tank.xPos)+1, tank.yPos, Black);
-		drawPixel((tank.xPos)+2, tank.yPos, Black);
-	}
+		canX = 0;
+		canY = -1;
+		}
+	else if(tank.aimDir == right
+		){
+		canX = 1;
+		canY = 0;
+		}
 	else if(tank.aimDir == down){
-		drawPixel(tank.xPos, (tank.yPos)+1, Black);
-		drawPixel(tank.xPos, (tank.yPos)+2, Black);
+		canX = 0;
+		canY = 1;
+		}
+	else{
+		//left
+		canX = -1;
+		canY = 0;
 	}
-	else {
-		drawPixel((tank.xPos)-1, tank.yPos, Black);
-		drawPixel((tank.xPos)-2, tank.yPos, Black);
+	for (i = (tank.xPos)-1; i <= tank.xPos+1; i++){
+		for (j = (tank.yPos)-1; j <=(tank.yPos)+1; j++){
+			if (i == tank.xPos  && j == tank.yPos)
+				drawPixel(i,j,Black);
+			else if (i == (tank.xPos)+canX && j == (tank.yPos)+canY){
+				drawPixel(i,j,Black);
+				drawPixel(i+canX,j+canY,Black);
+			}
+			else
+				drawPixel(i,j,Green);
+		}
 	}
-
+	drawPixel((tank.xPos)+canX, (tank.yPos)+canY,Black);
 }
 void clearTank(void){
 	drawPixel((tank.xPos)-1, (tank.yPos)-1, White);
@@ -111,7 +142,7 @@ __task void moveTank(void)
 		int button;
 		if((~LPC_GPIO1->FIOPIN & 0x07900000)){
 			button = (~LPC_GPIO1->FIOPIN);
-			while((~LPC_GPIO1->FIOPIN & 0x07900000));
+			//while((~LPC_GPIO1->FIOPIN & 0x07900000));
 			clearTank();
 				if(button & 0x04000000)
 					(tank.yPos)++; //down
@@ -129,7 +160,6 @@ __task void moveTank(void)
 	}
 }
 
-
 __task void start_tasks(void){
 	os_sem_init(&move, 1);
 	os_tsk_create(moveTank,1);
@@ -143,9 +173,7 @@ int main(void){
 	tank.aimDir = up;
 	tank.ammo = 8;
 
-// 	while(1){
-// 	printf("%d \n", rt_time_get());
-// 	}
+	printf(" df \n");
 	GLCD_Init();
 	GLCD_Clear(White);
 	GLCD_SetTextColor(0xFFFF);  
@@ -175,4 +203,6 @@ int main(void){
 	os_sys_init(start_tasks);
  
 }
+
+
 
